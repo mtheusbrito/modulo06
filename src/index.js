76979b1c87f3eaafcb3538c4a6cab09af14ab35b
matheus <matheusbritodasilva@gmail.com>
2019-12-31 17:17:01 -0300

    Configurando StatusBar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
-import React, {Component} from 'react'
-import {View} from 'react-native'
+import React from 'react'
+import {StatusBar} from 'react-native'
 import './config/ReactotronConfig'
 import Routes from './routes'
 
 export default function App(){
-    return <Routes/>
-};
+    return(
+          <>
+          <StatusBar barStyle='light-content' backgroundColor='#7159c1'/>
+          <Routes/>
+          </>
+          )};
