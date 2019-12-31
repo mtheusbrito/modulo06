482aef1b1773d0b62650173490bd1a025bbb21d8
matheus <matheusbritodasilva@gmail.com>
2019-12-31 17:10:06 -0300

    React Navigation

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import './config/ReactotronConfig';
+import React, {Component} from 'react'
+import {View} from 'react-native'
+import './config/ReactotronConfig'
+import Routes from './routes'
 
-export default class App extends Component{
-  render(){
-    return(
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-        Welcome to React Native!
-        </Text>
-      </View>
-      )
-  }
-}
-const styles = StyleSheet.create({
-  container:{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-  },
-  welcome:{
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
+export default function App(){
+    return <Routes/>
+};
