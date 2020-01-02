8180ec60b0cefd713656450c38bdb24c955c157e
matheus <matheusbritodasilva@gmail.com>
2020-01-02 15:37:55 -0300

    Listando favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container }  from './styles';
+import { Container, Header, Avatar, Name, Bio, Stars, Starred, OwnerAvatar, Info, Title, Author } from './styles';
 import PropTypes from 'prop-types';
 import api  from '../../services/api';
 
@@ -25,11 +25,35 @@ export default class User extends React.Component {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
     const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({stars: response.data});
   }
   render(){
+    const { navigation} = this.props;
+    const { stars } = this.state;
+
+    const user = navigation.getParam('user');
+
     return (
       <Container>
-        
+        <Header> 
+          <Avatar source={{ uri: user.avatar }}/>
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+        data={stars}
+        keyExtractor={star => String(star.id)}
+        renderItem={({item})=> (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }}/>
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+        ></Stars>
       </Container>
 
     );
