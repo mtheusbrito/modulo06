e482b6ae7a85b9c82461ac00a43457204266ed58
matheus <matheusbritodasilva@gmail.com>
2020-01-02 14:14:34 -0300

    buscando dados da API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import {Container }  from './styles';
 import PropTypes from 'prop-types';
+import api  from '../../services/api';
 
 export default class User extends React.Component {
+  static navigationOptions = ({ navigation }) =>({
+    title: navigation.getParam('user').name, 
+  });
+
+  static proptypes = {
+    navigation: PropTypes.shape({
+      getParam:PropTypes.func, 
+
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+
+  }
+
+
+  async componentDidMount(){
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    const response = await api.get(`/users/${user.login}/starred`);
+  }
   render(){
     return (
       <Container>
@@ -12,6 +35,4 @@ export default class User extends React.Component {
     );
   }
 }
-User.navigationOptions = {
-  title: 'Usuário',
-};
+
